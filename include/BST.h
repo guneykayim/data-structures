286c3c1b797201d6aea27232b0cae068d1528882
Guney Kayim <guneykayim@gmail.com>
2020-12-04 10:01:00 +0000

    added a binary search tree class - Not tested yet

---
+++ include/BST.h
@@ -0,0 +1,52 @@
+
+#ifndef BST_H
+#define BST_H
+
+#include <list>
+#include <string>
+
+using std::string;
+using std::list;
+
+template <typename T>
+class BST
+{
+public:
+
+    BST() = default;
+
+    ~BST() { if(root != nullptr) delete root; }
+
+    BST(const BST &another);
+
+    bool isEmpty() const;
+
+    bool add(string key, T value);
+
+    bool remove(string key);
+
+    T *get(string key) const;
+
+    list <T> *getBetweenRange(string start, string end) const;
+
+    const BST *findMin() const;
+
+    const BST *findMax() const;
+
+private:
+    template <typename S>
+    struct BSTNode
+    {
+        string key;
+        S value;
+
+        BST<S> left; // left sub-tree (left child)
+        BST<S> right; // right sub-tree (right child)
+        BSTNode(string key, S value) : key(key), value(value) {};
+    };
+
+    BSTNode<T> *root = nullptr;
+    void getBetweenRangeHelper(const BST<T> *current_bst, string start, string end, list <T> *resultList) const;
+};
+
+#endif //BST_H
---
+++ src/BST.cpp
@@ -0,0 +1,124 @@
+#include <BST.h>
+
+template <typename T>
+BST<T>::BST(const BST &another) { 
+    if(another.isEmpty()) {
+        return;
+    }
+    this->root = new BSTNode<T>(*another.root);
+}
+
+template <typename T>
+bool BST<T>::isEmpty() const { 
+    return this->root == nullptr;
+}
+
+template <typename T>
+bool BST<T>::add(string key, T value) { 
+    if(isEmpty()) {
+        this->root = new BSTNode<T>(key, value);
+        return true;
+    } else if(key.compare(this->root->key) == 0) {
+        return false;
+    } else if(key.compare(this->root->key) < 0 ) {
+        return this->root->left.add(key, value);
+    } else {
+        return this->root->right.add(key, value);
+    }
+}
+
+template <typename T>
+bool BST<T>::remove(string key) { 
+    if(isEmpty()){
+        return false;
+    } else if(key.compare(this->root->key) == 0){
+        if(this->root->left.root == nullptr && this->root->right.root == nullptr){ // no BSTs in right or left
+            this->root = nullptr;            
+        } else if(this->root->left.root != nullptr && this->root->right.root == nullptr){ // there is a BST in left
+            BSTNode<T> *tmp = this->root->left.root;
+            this->root = new BSTNode<T>(tmp->key, tmp->value);
+            this->root->left.root = tmp->left.root;
+            this->root->right.root = tmp->right.root; 
+        } else if(this->root->left.root == nullptr && this->root->right.root != nullptr){ // there is a BST in right
+            BSTNode<T> *tmp = this->root->right.root;
+            this->root = new BSTNode<T>(tmp->key, tmp->value);
+            this->root->left.root = tmp->left.root;
+            this->root->right.root = tmp->right.root;
+        } else{
+            const BST<T>* rightMinTree = this->root->right.findMin();
+            string tempkey = rightMinTree->root->key;
+            T tempvalue = rightMinTree->root->value;
+            this->root->right.remove(rightMinTree->root->key);
+            this->root->key = tempkey;
+            this->root->value = tempvalue;
+        }
+        
+        return true;
+        
+    } else if(key.compare(this->root->key) < 0){
+        return this->root->left.remove(key);
+    } else{
+        return this->root->right.remove(key);
+    }
+}
+
+template <typename T>
+T *BST<T>::get(string key) const {
+    if(this->root == nullptr) {
+        return nullptr;
+    }
+    if(this->root->key.compare(key) == 0) {
+        return &this->root->value;
+    } else if(key.compare(this->root->key) < 0){
+        return this->root->left.get(key);
+    } else{
+        return this->root->right.get(key);
+    }
+}
+
+template <typename T>
+list <T>* BST<T>::getBetweenRange(string start, string end) const {
+    if (start > end) {
+        string temp = end;
+        end = start;
+        start = temp;
+    }
+    list <T> *resultList = new list<T>;
+    this->getBetweenRangeHelper(this, start, end, resultList);
+
+    return resultList;
+}
+
+template <typename T>
+const BST<T>* BST<T>::findMin() const { 
+    if(this->root == nullptr) {
+        return nullptr;
+    } else if(this->root->left.root == nullptr) {
+        return this;
+    } else {
+        return this->root->left.findMin();
+    }
+}
+
+template <typename T>
+const BST<T>* BST<T>::findMax() const { 
+    if(this->root == nullptr) {
+        return nullptr;
+    } else if(this->root->right.root == nullptr) {
+        return this;
+    } else {
+        return this->root->right.findMax();
+    }
+}
+
+template <typename T>
+void BST<T>::getBetweenRangeHelper(const BST<T> *current_bst, string start, string end, list<T> *resultList) const {
+    if(current_bst->root == nullptr) {
+        return;
+    }
+    getBetweenRangeHelper(&(current_bst->root->left), start, end, resultList);
+    if(current_bst->root->key.compare(start) >= 0 && current_bst->root->key.compare(end) <= 0) {
+        resultList->emplace_back(current_bst->root->value);
+    }
+    getBetweenRangeHelper(&(current_bst->root->right), start, end, resultList);
+}
