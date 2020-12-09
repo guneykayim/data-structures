7e9a88ebd11dac1d088c7d257a5204c4c256d10a
Guney Kayim <guneykayim@gmail.com>
2020-12-09 23:04:12 +0000

    more tests added, for memory management

--- test/hashtable-test.cpp
+++ test/hashtable-test.cpp
@@ -9,21 +9,21 @@ int (*hash_func_int)(int) = [](int n) { return n * 2; };
 
 TEST(HashTableTestIsEmpty, testIsEmpty1) {
     int size = 10;
-    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, true);
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5);
     EXPECT_EQ(ht.size(), size);
     EXPECT_EQ(ht.count(), 0);
 }
 
 TEST(HashTableTestAdd, testAdd1) {
     int size = 10;
-    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, true);
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5);
     int *i = new int(5);
     EXPECT_EQ(ht.add("guney", i), 0);
 }
 
 TEST(HashTableTestIsEmpty, testIsEmpty2) {
     int size = 10;
-    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, true);
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5);
     int *i = new int(5);
     ht.add("guney", i);
     EXPECT_EQ(ht.count(), 1);
@@ -31,7 +31,7 @@ TEST(HashTableTestIsEmpty, testIsEmpty2) {
 
 TEST(HashTableTestAdd, testAdd2) {
     int size = 10;
-    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, true);
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5);
     int *i = new int(5);
     int *i2 = new int(10);
     ht.add("guney", i), 0;
@@ -40,13 +40,13 @@ TEST(HashTableTestAdd, testAdd2) {
 
 TEST(HashTableTestRemove, testRemove1) {
     int size = 10;
-    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, true);
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5);
     EXPECT_EQ(ht.remove("guney"), false);
 }
 
 TEST(HashTableTestRemove, testRemove2) {
     int size = 10;
-    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, true);
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5);
     int *i = new int(5);
     ht.add("guney", i);
     EXPECT_EQ(ht.remove("guney"), true);
@@ -54,7 +54,7 @@ TEST(HashTableTestRemove, testRemove2) {
 
 TEST(HashTableTestIsEmpty, testIsEmpty3) {
     int size = 10;
-    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, true);
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5);
     int *i = new int(5);
     ht.add("guney", i);
     ht.remove("guney");
@@ -63,7 +63,7 @@ TEST(HashTableTestIsEmpty, testIsEmpty3) {
 
 TEST(HashTableTestResize, testResize1) {
     int size = 10;
-    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, true);
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5);
     string s[] = {"g", "u", "n", "e", "y"};
     for (string e : s) {
         int *i = new int(e[0] - 'a' + 1);
@@ -81,10 +81,28 @@ TEST(HashTableTestResize, testResize1) {
 
 TEST(HashTableTestAdd, testAdd3) {
     int size = 10;
-    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, true);
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5);
     int *i = new int(5);
     int *i2 = new int(10);
     ht.add("guney", i), 0;
     EXPECT_EQ(ht.add("guney", i2), 1); 
-    EXPECT_EQ(ht.get("guney"), i2);
+    EXPECT_EQ(*ht.get("guney"), *i2);
+}
+
+TEST(HashTableMemoryManagement, testMemoryManagement1) {
+    int size = 10;
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, false);
+    int *i = new int(5);
+    ht.add("guney", i);
+    EXPECT_NE(i, ht.get("guney"));
+    EXPECT_EQ(*i, *ht.get("guney"));
+}
+
+TEST(HashTableMemoryManagement, testMemoryManagement2) {
+    int size = 10;
+    HashTable<string, int> ht(size, hash_func_str, hash_func_int, Mode::LINEAR, 0.5, true);
+    int *i = new int(5);
+    ht.add("guney", i);
+    EXPECT_EQ(i, ht.get("guney"));
+    EXPECT_EQ(*i, *ht.get("guney"));
 }
